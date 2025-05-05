49ab099eb88d5e41263b0ac6d33944243dd22ab5
parkpos <67667684+parkpos@users.noreply.github.com>
2025-05-05 14:22:26 +0900

    Update contract.dart
    전자결제 수정 (속성)

--- shoe_store_project/shoe_team_app/lib/model/contract.dart
+++ shoe_store_project/shoe_team_app/lib/model/contract.dart
@@ -1,52 +1,55 @@
 class Contract {
-    final String documentNo; // ED,년월일(6),SEQNO(2)
-    final String productCode;
-    final String startEmployeeNo;
-    final String middleEmployeeNo;
-    final String endEmployeeNo;
-    final String documentTitle;
-    final String documentDetail;
-    final int maxQuantity;
-    final int approvalState;
-    final int orderingPrice;
-    final String stockDate;
-    final String moveDate;
-    final int quantity;
-    final int orderState;
-    final String storeCode;
+  final String documentNo; // 전자결제번호 : ED,년월일(6),SEQNO(2)
+  final String writeDate; // 작성일
+  final String productCode; // 제품코드
+  final String startEmployeeNo; // 사원번호
+  final String middleEmployeeNo; // 중간결제자
+  final String endEmployeeNo; // 최종결제자
+  final String documentTitle; // 전자결제 제목
+  final String documentDetail; // 전자결제 상세
+  final int? maxQuantity; // 재고수량 Max value
+  final int approvalState; // 전자결제 상태
+  final int orderingPrice; // 반품금액/발주단가
+  final String? stockDate; // 입고일자 (본사 <- 제조사)
+  final String? moveDate; // 대리점 이동일자
+  final int quantity; // 반품수량/수발수량
+  final int? orderState; // 발주상태
+  final String storeCode; // 매장코드드
 
   Contract({
     required this.documentNo,
+    required this.writeDate,
     required this.productCode,
     required this.startEmployeeNo,
     required this.middleEmployeeNo,
     required this.endEmployeeNo,
     required this.documentTitle,
     required this.documentDetail,
-    required this.maxQuantity,
+    this.maxQuantity,
     required this.approvalState,
     required this.orderingPrice,
-    required this.stockDate,
-    required this.moveDate,
+    this.stockDate,
+    this.moveDate,
     required this.quantity,
-    required this.orderState,
-    required this.storeCode
+    this.orderState,
+    required this.storeCode,
   });
 
-  Contract.fromMap(Map<String,dynamic> res)
-  :documentNo = res['documentNo'],
-  productCode = res['productCode'],
-  startEmployeeNo = res['startEmployeeNo'],
-  middleEmployeeNo = res['middleEmployeeNo'],
-  endEmployeeNo = res['endEmployeeNo'],
-  documentTitle = res['documentTitle'],
-  documentDetail = res['documentDetail'],
-  maxQuantity = res['maxQuantity'],
-  approvalState = res['approvalState'],
-  orderingPrice = res['orderingPrice'],
-  stockDate = res['stockDate'],
-  moveDate = res['moveDate'],
-  quantity = res['quantity'],
-  orderState = res['orderState'],
-  storeCode = res['storeCode'];
-} 
+  Contract.fromMap(Map<String, dynamic> res)
+    : documentNo = res['documentNo'],
+      writeDate = res['writeDate'],
+      productCode = res['productCode'],
+      startEmployeeNo = res['startEmployeeNo'],
+      middleEmployeeNo = res['middleEmployeeNo'],
+      endEmployeeNo = res['endEmployeeNo'],
+      documentTitle = res['documentTitle'],
+      documentDetail = res['documentDetail'],
+      maxQuantity = res['maxQuantity'],
+      approvalState = res['approvalState'],
+      orderingPrice = res['orderingPrice'],
+      stockDate = res['stockDate'],
+      moveDate = res['moveDate'],
+      quantity = res['quantity'],
+      orderState = res['orderState'],
+      storeCode = res['storeCode'];
+}
